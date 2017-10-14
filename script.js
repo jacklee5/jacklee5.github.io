16d13b7359978d46615af23f971a4a14d01de231
Jack Lee <jack200336@gmail.com>
2017-10-14 18:21:15 -0400

    auto-update home

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*
+<a href = "https://jacklee5.github.io/DINGDONG" target = "blank">
+                <div class = "card">
+                    <h1>DiIIiIiINnNNNgGGgGgGg DDDDDDdDdOoOOonnnNNgggGgg</h1>
+                </div> 
+            </a>*/
+var addProject = function(title,url){
+    document.getElementById("projects").innerHTML += "<a href = '"+url+"' target = 'blank'><div class = 'card'><h1>"+title+"</h1></div></a>";
+}
+firebase.database().ref("projects").once("value",function(snapshot){
+    for(var i in snapshot.val()){
+        addProject(snapshot.val()[i].title,snapshot.val()[i].url);
+    }
+})
 var pages = document.querySelectorAll(".page");
 var active = 0;
 var navLinks = document.querySelectorAll("nav a")
