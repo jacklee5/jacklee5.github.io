be1f031a5bbce2e7e030d5d04789334b9f7542af
Jack Lee <jack200336@gmail.com>
2017-12-01 21:12:54 -0500

    nvm

--- BCA-ActiveFloor-master/BCA-ActiveFloor-master/Memory/screens.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Cassandra on 7/26/2016.
- */
-function endScreen() {
-    context2D.fillStyle = 'white';
-    context2D.font = '16px sans-serif';
-    context2D.fillText("Game Over",(canvas.width/2 - (context2D.measureText('Game Over').width / 2)), canvas.height/2),
-        75;
-}
--- BCA-ActiveFloor-master/BCA-ActiveFloor-master/README.md
+++
@@ -1,2 +0,0 @@
-# BCA-ActiveFloor
-When configuring window, configure Visual Effects to "Adjust for best performance".  Under Windows 10, this option can be found in Control Panel->System->Advanced system settings.
